0f2bbc2709ebbb426255e741e3b8816ffcd11dc4
KehongChen2005 <chenkh5257@gmail.com>
2024-09-30 02:56:12 +0800

    Create test.1.c

---
+++ test.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main();
+int num=1;
+for(num=1;num<=250;num++){
+    if(num%3==0)
+    printf("Fizz");
+    elseif(num%5==0)
+    printf("Fizzy");
+    elseif(num%7==0)
+    printf("Buzz");
+    printf(", ")
+}
+return 0;
